0c0b7b362c1db60f01b6c6924dcc8f0e3c5e9b32
Gaudhiwaa Hendrasto <gaudhiwaa@gmail.com>
2021-09-11 05:20:37 +0700

    Project Setup

---
+++ warung.io/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap-5.1.1-dist/css/bootstrap.min.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
