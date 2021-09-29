ea331468d69ea02976987c3070873fe4d9ffb0a9
Gaudhiwaa Hendrasto <gaudhiwaa@gmail.com>
2021-09-29 15:22:47 +0700

    Resolve [UPDATE] Copywriting Halaman Gaya Bahasa

--- warung.io/src/App.js
+++ warung.io/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import 'bootstrap-5.1.1-dist/css/bootstrap.min.css'
-
+import 'bootstrap-5.0.2-dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
+import Description from './components/Description';
+import Partner from './components/Partner';
+import Feature from './components/Feature';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello!</h1>
+        <Navbar/>
+        <Welcome/>
+        <Description/>
+        <Partner/>
+        <Feature/>
       </header>
     </div>
   );
